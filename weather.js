d25f0f9328615ee17c99106f967feb8d55b5c39f
aidencoders <77876142+aidencoders@users.noreply.github.com>
2021-02-08 23:52:57 +0900

    add: Getting the Weather
    openweather API를 이용하여 위도 경도로 fetch,
    
    string으로 온 response를 json으로 변경하여 현재 위치, 온도를 표시하는 기능 추가

---
+++ weather.js
@@ -0,0 +1,56 @@
+const COORDS = "coords";
+const API_KEY = "430aed0bbd99cc012ef47f25b2c71014";
+const weather = document.querySelector(".js-weather");
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    //getWeather
+    const parsedCoords = JSON.parse(loadedCords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+function init() {
+  loadCoords();
+}
+init();
